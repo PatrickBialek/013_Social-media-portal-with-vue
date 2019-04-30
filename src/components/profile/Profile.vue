d06eddd6a2b86b1445f7f7e32bdba6f6e6fc7997
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-05-01 01:51:37 +0200

    Working on styles in user profile part 1

--- src/components/profile/Profile.vue
+++ src/components/profile/Profile.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="user">
     <div v-if="profile" class="user__content">
-      <h2 class="user__name">{{ title }}</h2>
+      <h2 class="user__name">{{ title }}'s wall</h2>
       <div class="user__references">
-        <h3>User references:</h3>
         <form class="user__form" @submit.prevent="addReference">
-          <label class="user__label" for="add-reference"></label>
+          <label class="user__label" for="add-reference">Your reference:</label>
           <textarea
-            class="user__text-field"
+            class="user__textarea"
             type="text"
             name="add-reference"
             id="add-reference-textarea"
@@ -16,7 +15,9 @@
           <button class="user__btn">Add reference</button>
           <p class="user__error" v-if="feedback">{{ feedback }}</p>
         </form>
-        <div class="user__references-box"></div>
+        <div class="user__references-box">
+          <h3>User references:</h3>
+        </div>
       </div>
     </div>
   </div>
@@ -94,6 +95,91 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.user {
+  padding: 50px 20px;
+
+  &,
+  &__content,
+  &__references,
+  &__form {
+    display: flex;
+    width: 100%;
+    box-sizing: border-box;
+    justify-content: center;
+  }
+
+  &__content {
+    max-width: 1200px;
+  }
+
+  &__content,
+  &__references {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  &__name {
+    font-size: 20px;
+    color: #303030;
+    font-weight: 700;
+    font-family: Rubik, Arial, Helvetica, sans-serif;
+    margin-bottom: 15px;
+  }
+
+  &__references {
+  }
+
+  &__form {
+    justify-content: center;
+    flex-direction: column;
+    text-align: left;
+    box-sizing: border-box;
+    align-items: flex-start;
+  }
+
+  &__label {
+    margin-bottom: 4px;
+    font-size: 15px;
+    line-height: 1.7;
+    color: #7d7c7c;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+
+  &__textarea {
+    resize: none;
+    width: 100%;
+    height: 100px;
+    border: 1px solid #e4e4e4;
+    background-color: #fff;
+    outline: none;
+    padding: 8px 12px;
+    font-size: 16px;
+    color: #303030;
+  }
+
+  &__btn {
+    display: block;
+    background-color: #428bca;
+    color: #fff;
+    padding: 8px 12px;
+    cursor: pointer;
+    border: none;
+    font-size: 16px;
+    margin-top: 10px;
+    -webkit-transition: background-color 0.2s ease-in-out;
+    transition: background-color 0.2s ease-in-out;
+
+    &:hover {
+      background-color: #186eb9;
+    }
+  }
+
+  &__error {
+  }
+
+  &__references-box {
+  }
+}
 </style>
 
 
