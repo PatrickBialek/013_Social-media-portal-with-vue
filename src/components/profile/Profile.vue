8eada4a5f8c6a7b87b410c7e7ca63bd46035ce67
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-04-30 23:47:55 +0200

    Working on reference layout part 1

--- src/components/profile/Profile.vue
+++ src/components/profile/Profile.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="user">
-    <div v-if="profile" class="user__content">{{ title }}'s wall</div>
+    <div v-if="profile" class="user__content">
+      <h2 class="user__name">{{ title }}</h2>
+      <div class="user__references">
+        <h3>User references:</h3>
+        <form class="user__form" @submit.prevent="addReference">
+          <label class="user__label" for="add-reference"></label>
+          <input class="user__text-field" type="text" name="add-reference" v-model="newReference">
+          <p class="user__error" v-if="feedback">{{ feedback }}</p>
+        </form>
+        <div class="user__references-box"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,7 +23,9 @@ export default {
   data() {
     return {
       profile: null,
-      title: null
+      title: null,
+      newReference: null,
+      feedback: null
     };
   },
   created() {
@@ -28,6 +41,9 @@ export default {
           this.profile.nick.charAt(0).toUpperCase() +
           this.profile.nick.slice(1);
       });
+  },
+  methods: {
+    addReference() {}
   }
 };
 </script>
